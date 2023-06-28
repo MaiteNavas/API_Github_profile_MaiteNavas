290aa2e9b6a9413d62bf30de980142925622713b
MaiteNavas <maitenavas626@gmail.com>
2023-06-28 09:41:01 +0200

    entrega taller

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const search = document.getElementById('search');
-const profileContainer = document.getElementById('profile');
+
+const profileContainer = document.getElementById('main');
+
 const mainContainer = document.createElement('div');
+mainContainer.classList.add('card');
+
 const avatar = document.createElement('img');
+avatar.classList.add('avatar');
+
 const userInfo = document.createElement('div');
+userInfo.classList.add('user-info');
+
 const userName= document.createElement('h2');
 const userDescription = document.createElement('p');
+
 const infoExtra = document.createElement('ul');
 const followers = document.createElement('li');
 const following = document.createElement('li');
 const totalRepos = document.createElement('li');
-const repos = document.createElement('div');
+
 const repo1 = document.createElement('p');
+repo1.classList.add('repo');
 const repo2 = document.createElement('p');
+repo2.classList.add('repo');
 const repo3 = document.createElement('p');
-mainContainer.classList.add('card');
-avatar.classList.add('avatar');
-userInfo.classList.add('user-info');
-repo1.classList.add('repo')
-repo2.classList.add('repo')
-repo3.classList.add('repo')
+repo3.classList.add('repo');
+const repo4 = document.createElement('p');
+repo4.classList.add('repo');
+const repo5 = document.createElement('p');
+repo5.classList.add('repo');
+
 profileContainer.appendChild(mainContainer)
 mainContainer.appendChild(avatar);
 mainContainer.appendChild(userInfo);
@@ -28,12 +39,11 @@ userInfo.appendChild(infoExtra);
 infoExtra.appendChild(followers);
 infoExtra.appendChild(following);
 infoExtra.appendChild(totalRepos);
-userInfo.appendChild(repos);
-repos.appendChild(repo1);
-repos.appendChild(repo2);
-repos.appendChild(repo3);
-
-
+userInfo.appendChild(repo1);
+userInfo.appendChild(repo2);
+userInfo.appendChild(repo3);
+userInfo.appendChild(repo4);
+userInfo.appendChild(repo5);
 
 const endPoint = ('https://api.github.com/users/')
 
@@ -42,125 +52,42 @@ async function getUser(user){
     const userUrl = `${endPoint}${user}`;
     const response = await fetch(userUrl);
     const data = await response.json();
-   console.log(data);
+    console.log(data);
     createUserCard(data)
 
     function createUserCard(user){
 
-
-        
         userName.innerHTML = `${data.name}`;
         avatar.src= `${data.avatar_url}`;
         userDescription.innerHTML = `${data.bio}`
         followers.innerHTML = `Followers ${data.followers}`
         following.innerHTML = `Following ${data.following}`
         totalRepos.innerHTML = `Repos ${data.public_repos}`
-        
-        
-    
     }
-    getRepoUser(user)
-        
-        
+    getRepoUser(user)    
 }
 
 async function getRepoUser(user){
         
         const repoUrl = `${endPoint}${user}/repos`;
         const responseRepo = await fetch(repoUrl);
-        const dataRepo = await responseRepo.json()
-        console.log(dataRepo)
-        repo1.innerHTML = `${dataRepo[1].name}`
-        repo2.innerHTML = `${dataRepo[2].name}`
-        repo3.innerHTML = `${dataRepo[3].name}`
-
-
+        const dataRepo = await responseRepo.json();
+        //console.log(dataRepo)
+        const newDataRepo = dataRepo.sort((a, b) => a.stargazers_count - b.stargazers_count)
+        .slice(0,5)   
+        console.log(newDataRepo)   
+        repo1.innerHTML = `${newDataRepo[0].name}`;
+        repo2.innerHTML = `${newDataRepo[1].name}`;
+        repo3.innerHTML = `${newDataRepo[2].name}`;
+        repo4.innerHTML = `${newDataRepo[3].name}`;
+        repo5.innerHTML = `${newDataRepo[4].name}`;  
 }
-
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const user = search.value;
-
     if (user) {
         getUser(user);
-
         search.value = "";
     }
 });
-
-
-
-
-
-//console.log(dataUser);
-//dataUser.addEventListener("click", getUser(inputUser.value));
-
-/*
-
-        
-
-
-const inputUser = document.getElementById('search');
-inputUser.addEventListener('submit', function(e) {
-  var keycode = e.keyCode || e.which;
-  if (keycode == 13) {
-    getUser()
-  }
-});
-
-console.log(inputUser.value)
-
-
-
-
-
-async function getUser(){
-    const userUrl = `${endPoint}${dataUser}`;
-    const repoUrl = `${endPoint}${dataUser}/repos`;
-    try{
-        const data = await fetch(userUrl);
-        console.log(data)
-        
-    } catch (error) {
-
-    }
-
-}
-
-//inputUser.addEventListener("click", getUser(inputUser.value));
-
-*/
-/*const url = 'https://api.github.com/users/'
-const config = {
-    method: 'GET',
-    headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-    },
-    }
-fetch(url, config)
-.then((response) => response.json())
-.then((data) => {
-    console.log(data)
-
-}).catch((error) => {
-        console.error(error);
-    });
-
-*/
-
-/*async function getUsers(){
-
-    const config = {
-    headers: {
-        Accept:'application/vnd.github+json',
-    },
-    }
-    let response = await fetch("https://api.github.com/users/", config)
-
-    let data = await response.json()
-    console.log(data)
-
-}
-getUsers()*/
